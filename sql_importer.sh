d0c7ab1690cdf9aa6a126ed4f55bc907c4ce6ea1
DrShams <sithis02@mail.ru>
2022-03-15 04:08:33 +0500

    sql_importer added

--- sql_importer.sh
+++ sql_importer.sh
@@ -1,6 +1,6 @@
 docker-compose -f prom_grafana.yaml up
 docker-compose -f postgres_exp.yaml up
 #postgre sql container_name
-docker exec -i f04feb3303ee psql -U ruslan -d demo < demo.sql
-#http://192.168.19.128:3000/
+docker exec -i [container_postgre_id] psql -U ruslan -d demo < demo.sql
+#http://192.168.19.128:3000/ - ип адрес хоста
 #grafana id 9628
